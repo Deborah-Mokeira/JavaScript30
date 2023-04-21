20bbffe59cf5cd71ade41c49dbc21d4104f75cef
Deborah <119611300+Deborah-Mokeira@users.noreply.github.com>
2023-04-21 13:48:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,22 +7,18 @@ function removeTransition(e) {
 
 // This function plays a sound and adds the 'playing' class to the corresponding key.
 function playSound(e) {
-  const audio = document.querySelector(audio[data-key="${e.keyCode}"]);
-  const key = document.querySelector(div[data-key="${e.keyCode}"]);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   // Stop the function if the pressed key does not correspond to an audio file.
   if (!audio) return;
 
   key.classList.add('playing');
   audio.currentTime = 0;
   audio.play();
-
- // Remove class 'playing' after some time
- setTimeout(() => {
-  key.classList.remove('playing');
-}, 300); // same duration as in the CSS rule '.key.playing'
 }
+
 // Get all the key elements on the page and add a 'transitionend' event listener to each one.
-const keys = document.querySelectorAll('.key');
+const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 // Add a 'keydown' event listener to the window to trigger the playSound function when a key is pressed.
